f15f43d1fa98204f276c2cc1d54b59aeea13160b
HyunSu Lim <hyunsu.lim01@gmail.com>
2018-06-13 16:07:14 +0900

    [feat] Add schema for Diff table

--- models.py
+++ models.py
@@ -21,6 +21,14 @@ class Commit(Base):
     timestamp = Column(Integer)
     count = Column(Integer)  # Recent commit has lower count value
 
+class Diff(Base):
+    __tablename__ = 'diff'
+
+    project_id = Column(String, ForeignKey(Project.id), primary_key=True)
+    commit_hash = Column(String, ForeignKey(Commit.hash), primary_key=True)
+    path = Column(String, primary_key=True)
+    hunks = Column(PickleType)  # List[(old_start, old_lines, new_start, new_lines)]
+
 
 class File(Base):
     __tablename__ = 'file'
